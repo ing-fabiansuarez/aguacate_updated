e338d7d05c99895a0f7ba0248589142f767aab35
Fabian Suarez <fsuarez120@unab.edu.co>
2021-12-03 02:18:36 -0500

    cambios dia sin iva

--- app/Views/ecommerce/home.php
+++ app/Views/ecommerce/home.php
@@ -16,13 +16,14 @@
         <div class="item active">
             <div class="container">
                 <div class="carousel-caption">
-                    <h3>Ropa Nueva <span>Todos los fines de semana</span></h3>
-                    <p>Nuevas Colecciones</p>
-                    <a class="hvr-outline-out button2" href="<?= base_url() . route_to('section_new_ecommerce') ?>">Lo quiero todo </a>
+                    <!--   <h3>Ropa Nueva <span>Todos los fines de semana</span></h3>
+                        <p>Nuevas Colecciones</p>
+                        <a class="hvr-outline-out button2" href="<?= base_url() . route_to('section_new_ecommerce') ?>">Lo quiero todo </a>
+                -->
                 </div>
             </div>
         </div>
-        <div class="item item2">
+        <!--  <div class="item item2">
             <div class="container">
                 <div class="carousel-caption">
                     <h3>Ropa Nueva <span>Todos los fines de semana</span></h3>
@@ -30,7 +31,7 @@
                     <a class="hvr-outline-out button2" href="<?= base_url() . route_to('section_new_ecommerce') ?>">Comprar ahora </a>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- <div class="item item3">
 				<div class="container">
 					<div class="carousel-caption">
@@ -93,7 +94,7 @@
             <a href="<?= base_url() . route_to('view_categories_section', 'vlargos') ?>">
                 <div class="bb-middle-agileits-w3layouts forth grid">
 
-                     <figure class="effect-roxy">
+                    <figure class="effect-roxy">
                         <img src="assets/img/ecommerce/home/category1-2.jpeg" alt=" " class="img-responsive" />
                         <figcaption>
                             <h3><span>V</span>estidos Cortos </h3>
